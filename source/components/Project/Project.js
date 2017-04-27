2365c6cde8ca55c1bf9100eb8dfd5030c04b55c9
lizzkats <katsnelson.lizz@gmail.com>
2017-04-27 09:51:42 -0700

    moo

--- source/components/Project/Project.js
+++ source/components/Project/Project.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Heading from '../reusable/Heading/Heading'
 import RowList from '../reusable/Row/RowList'
 import FooterContainer from '../reusable/Footer/FooterContainer'
+import { Link } from 'react-router'
 
 const Project = ({ couldDos, project }) => (
   <div className='project-container' >
     <Heading type='project' text={ project.text } />
     <RowList type='could-do' items={ couldDos } />
+    <Link to='/project'><Icon type='back' /></Link>
     <FooterContainer type='could-do' />
   </div>
 )
