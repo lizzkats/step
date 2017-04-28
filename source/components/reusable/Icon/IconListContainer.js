1bd143e3a66231efea7392332fecb1589ec1ffc7
lizzkats <katsnelson.lizz@gmail.com>
2017-04-28 11:20:35 -0700

    adds delete function and tests

--- source/components/reusable/Icon/IconListContainer.js
+++ source/components/reusable/Icon/IconListContainer.js
@@ -1,26 +1,36 @@
 /* global __HOST__ */
-
-import React from 'react'
+import React, { Component } from 'react'
 import axios from 'axios'
-import GlobalStateComponent from '../../reusable/ParentClasses/GlobalStateComponent'
 import componentErrorHandler from '../../utilities/componentErrorHandler'
 import globalState from '../../utilities/globalState'
 import IconList from './IconList'
 
-export default class IconListContainer extends GlobalStateComponent {
-  componentDidMount() {
-    axios.get( `${__HOST__}/user/${this.props.icons}/projects` )
-      .then( ({ data: icons }) => {
-        const iconsObject = icons.reduce( ( accumulator, { id, text }) =>
-        Object.assign( accumulator, { [id]: { id, text } })
-        , {})
-        globalState.set({ icons: iconsObject })
+export default class IconListContainer extends Component {
+  constructor( props ) {
+    super( props )
+    this.deleteItems = this.deleteItems.bind( this )
+  }
+
+  deleteItems() {
+    const { type, id } = this.props
+
+    axios.post( `${__HOST__}/${type}/delete/${id}` )
+      .then( () => {
+        switch ( type ) {
+          case 'project':
+            globalState.deleteProject( id )
+            break
+          case 'could-do':
+            globalState.deleteCouldDo( id )
+            break
+          default:
+        }
       })
       .catch( componentErrorHandler( 'IconListContainer' ) )
   }
 
 
   render() {
-    return <IconList />
+    return <IconList deleteItems={ this.deleteItems } />
   }
 }
